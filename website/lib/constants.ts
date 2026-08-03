b0e240ab660d346cb6dfc20e8fd33833260ee6b7
sjgod1427 <sambhav.j23@iiits.in>
2026-08-03 19:45:29 +0530

    fix: point GitHub nav/footer links to the real repository

--- website/lib/constants.ts
+++ website/lib/constants.ts
@@ -1,3 +1,1 @@
-// TODO: replace with the real repository URL once one exists and the user
-// has said it's OK to make this public. Do not fill this in with a guess.
-export const GITHUB_URL = "#";
+export const GITHUB_URL = "https://github.com/sjgod1427/PaperPilot";
